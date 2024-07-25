25c64666d118e7210cf21889a9dbf01b64c45e6d
lipithat <97915798+lipithat@users.noreply.github.com>
2024-07-25 11:26:57 -0700

    minor changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,27 @@
 // Footer.js
 import './Footer.css'
+import { Link } from 'react-router-dom';
+import { RiGithubLine } from "react-icons/ri";
+import { PiLinkedinLogoBold } from "react-icons/pi";
+import { MdOutlineEmail } from "react-icons/md";
+
 export default function Footer() {
 	return (
 		<section className="footer-section">
 			<div className="footer">
 				<p className="footer-text">
 					© {new Date().getFullYear()} Lipitha Tummala.
+					<div className="logo">
+						<Link to="https://github.com/lipithat">
+							<RiGithubLine className="github-icon" size={25} />
+						</Link>
+						<Link to="https://www.linkedin.com/in/lipitha-tummala/">
+							<PiLinkedinLogoBold className="linkedin-icon" size={25} />
+						</Link>
+						<Link to="mailto:lipithatummala@gmail.com">
+							<MdOutlineEmail className="mail-icon" size={28} />
+						</Link>
+					</div>
 				</p>
 			</div>
 		</section>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,9 @@
 // Hero.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { VscGithubAlt } from "react-icons/vsc";
+import { RiGithubLine } from "react-icons/ri";
+import { PiLinkedinLogoBold } from "react-icons/pi";
+import { MdOutlineEmail } from "react-icons/md";
 
 import './Hero.css';
 
@@ -18,11 +20,20 @@ export default function Hero() {
 					I'm a student at UC Santa Cruz and a software engineer at <Link to="https://www.fluviq.com/" className="link">FLUVIQ</Link>
 				    .<br /><br />
 					Take a look at my resume.<br /><br />
-					<Link to="https://github.com/lipithat" className="logo">
-						<VscGithubAlt size={32} />
-					</Link>
+					<div className="logo">
+						<Link to="https://github.com/lipithat">
+							<RiGithubLine className="github-icon" size={25} />
+						</Link>
+						<Link to="https://www.linkedin.com/in/lipitha-tummala/">
+							<PiLinkedinLogoBold className="linkedin-icon" size={25} />
+						</Link>
+						<Link to="mailto:lipithatummala@gmail.com">
+							<MdOutlineEmail className="mail-icon" size={28} />
+						</Link>
+					</div>
 				</p>
 			</div>
+			<div className="linebreak"></div>
 		</section>
 	);
 }
